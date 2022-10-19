fc4e4b43a53a4d65c372043246c48fb4bbdfa751
sarahdlcrz02 <114977923+sarahdlcrz02@users.noreply.github.com>
2022-10-19 11:05:43 +0800

    Add files via upload

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 from flask import *
 app = Flask(__name__)
 
-@app.route('/table/<int:num>')
-def table (num):
-    return render_template('print-table.html', n =num)
-if __name__=='_main_':
+@app.route('/')
+def message():
+    return render_template('message.html')
+if __name__ == '__main__':
     app.run(debug = True)
